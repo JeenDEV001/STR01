286180215bf4f2558295aba99b896742bb6cba49
JeenDEV001 <chian2544@gmail.com>
2023-12-15 14:29:13 +0700

    .commit

--- home.py
+++ home.py
@@ -6,6 +6,9 @@ st.header('สาร์ทจีน บูรณะสุวรรณ')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
 
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
 with col1:
     st.image('./pic/jeen.jpg')
 with col2:
