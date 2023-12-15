12f3eebb822468adf8272557eac15a70404fe691
JeenDEV001 <chian2544@gmail.com>
2023-12-15 14:26:58 +0700

    .commit

--- home.py
+++ home.py
@@ -5,3 +5,8 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('สาร์ทจีน บูรณะสุวรรณ')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
+with col1:
+    st.image('./pic/jeen.jfif')
+with col2:
+    st.image('./pic/iris.jpg')
