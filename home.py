b3820600955654cbde54853d9bb4373022696609
JeenDEV001 <chian2544@gmail.com>
2023-12-15 14:21:20 +0700

    .commit

--- home.py
+++ home.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.header("Sartjeen Buranasuwan")
+
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('สาร์ทจีน บูรณะสุวรรณ')
+st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
